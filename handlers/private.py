173588177e3936df1a85a3180215dc7a3cb96760
ABHIMUSICTEAM <108426381+ABHIMUSICTEAM@users.noreply.github.com>
2022-12-12 16:16:21 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -10,7 +10,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 @Client.on_message(filters.command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://te.legra.ph/file/505bda41f97d408c7dd2c.jpg",
+        photo=f"https://te.legra.ph/file/308e874f6021d77458941.mp4",
         caption=f"""**ᴛʜɪꜱ ᴍᴜꜱɪᴄ ʙᴏᴛ ɪꜱ ᴀᴅᴠᴀɴᴄᴇ ᴀɴᴅ ɴᴏ ʟᴀɢ ᴜꜱᴇ ɪᴛ ᴄᴏɴᴛɪɴᴜᴏᴜꜱʟʏ .
 ᴄᴏᴅᴇʀ :- [ʙᴀᴅᴇ ʟᴏɢ](https://t.me/nobitadev)
 ɪꜰ ʏᴏᴜ ᴀɴʏ ᴘʀᴏʙʟᴇᴍ ᴛᴇʟʟ ʜᴇʀᴇ = [ʙᴀᴅᴇ ʟᴏɢ](https://t.me/nobitadev)**""",
